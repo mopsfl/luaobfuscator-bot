5a08a6f4bb20b37dd21db642ced1a32074e1e338
Flo <floda854@gmail.com>
2023-05-04 15:02:18 +0200

    fixed rate limit after few mins (forgot to set back xd)

--- .config.js
+++ .config.js
@@ -2,7 +2,7 @@ let prefix = "!"
 
 module.exports = {
     prefix: prefix, // Prefix for commands
-    activity_update_interval: 5, // Update interval for the bot activity
+    activity_update_interval: 10000, // Update interval for the bot activity
     dm_commands: true, // If dm commands should be allowed 
     activities: [ // Bot activities
         `${prefix}obf <code | file>`,
