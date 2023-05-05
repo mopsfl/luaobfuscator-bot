57b6dbafd575878555283d6863a93424cc51ebfd
Flo <floda854@gmail.com>
2023-05-05 19:18:12 +0200

    added obfuscation functions

--- commands/obfuscate.js
+++ commands/obfuscate.js
@@ -1,10 +1,10 @@
 const { createEmbed } = require("../utils/embed")
 const { sendErrorMessage } = require("../utils/command")
-const { Colors, codeBlock, blockQuote } = require("discord.js")
+const { Colors, codeBlock, blockQuote, inlineCode, hyperlink, Attachment } = require("discord.js")
 const { getEmoji } = require("../utils/misc")
 const config = require("../.config")
 const fetch = require("fetch")
-const { parseCodeblock, hasCodeblock, hasWebhook } = require("../utils/obfuscate-util")
+const { parseCodeblock, hasCodeblock, hasWebhook, createSession, parseWebhooks, manualObfuscateScript, obfuscateScript, createFileAttachment } = require("../utils/obfuscate-util")
 
 module.exports = {
     enabled: true,
@@ -23,11 +23,11 @@ module.exports = {
             message = arg.message || arg
 
         if (!message) return
+        let script = null, haswebhook
         const iscodeblock = hasCodeblock(arg.rawargs)
-        const haswebhook = hasWebhook(arg.rawargs)
         if (message.content.includes("```") && iscodeblock) {
-            const script = parseCodeblock(arg.rawargs)
-            console.log(script)
+            haswebhook = hasWebhook(arg.rawargs)
+            script = parseCodeblock(arg.rawargs)
         } else if ([...message.attachments].length > 0) {
             const attachment = message.attachments.first()
             const url = attachment ? attachment.url : null
@@ -47,8 +47,8 @@ module.exports = {
                     console.error(error)
                     return
                 }
-                const script = body.toString()
-                //obfuscate script
+                script = body.toString()
+                if (typeof script == "string") haswebhook = hasWebhook(script)
             })
         } else {
             let usage_args = arg.props.arguments.length > 0 ? "`" + `${arg.props.arguments}` + "`" : ""
@@ -61,9 +61,117 @@ module.exports = {
                 fields: [
                     { name: "Syntax:", value: `${usage_cmd} ${usage_args}` },
                     { name: "Reminder:", value: `If you need help, go to <#1083105006506487919> or ask in <#1069422456579829822>` }
-                ]
+                ], footer: {
+                    text: "LuaObfuscator Bot • made by mopsfl#4588",
+                    iconURL: config.ICON_URL
+                }
             })
             return message.reply({ embeds: [error_embed] })
         }
+
+        let process_embed = createEmbed({
+            fields: [
+                { name: `Obfuscation Process`, value: `${getEmoji("loading")} Creating session...` }
+            ],
+            timestamp: true,
+            color: Colors.Yellow,
+            footer: {
+                text: "LuaObfuscator Bot • made by mopsfl#4588",
+                iconURL: config.ICON_URL
+            }
+        })
+
+        if (haswebhook) {
+            const webhooks = parseWebhooks(arg.rawargs)
+            let embed_webhook_string = ""
+            for (let i = 0; i < webhooks.length; i++) {
+                if (!embed_webhook_string.includes(webhooks[i].trim())) {
+                    embed_webhook_string = embed_webhook_string + codeBlock("lua", webhooks[i].trim())
+                }
+            }
+            switch (config.script_scan_options.discord_webhooks) {
+                case "block": {
+                    return message.author.send({
+                        embeds: [createEmbed(
+                            {
+                                title: `${getEmoji("failed")} Obfuscation cancelled`,
+                                fields: [
+                                    {
+                                        name: "Problems:",
+                                        value: `${codeBlock("diff", "- Please remove all discord webhooks from your script.")}`
+                                    },
+                                    {
+                                        name: `Webhooks found: (${webhooks.length})`,
+                                        value: embed_webhook_string
+                                    },
+                                ],
+                                color: Colors.Red,
+                                timestamp: true,
+                                footer: {
+                                    text: "LuaObfuscator Bot • made by mopsfl#4588",
+                                    iconURL: config.ICON_URL
+                                }
+                            }
+                        )]
+                    })
+                    break
+                }
+                case "warn": {
+                    message.author.send({
+                        embeds: [createEmbed(
+                            {
+                                title: `${getEmoji("warn")} Webhook detected`,
+                                fields: [
+                                    {
+                                        name: "Note:",
+                                        value: `${"We've detected that your script has discord webhooks implemented.\nUsing them for malicious purposes (e.g. Scams, IP Loggers, ...) can result in a punishment."}`
+                                    },
+                                    {
+                                        name: `Webhooks found: (${webhooks.length})`,
+                                        value: embed_webhook_string
+                                    },
+                                ],
+                                color: Colors.Red,
+                                timestamp: true,
+                                footer: {
+                                    text: "LuaObfuscator Bot • made by mopsfl#4588",
+                                    iconURL: config.ICON_URL
+                                }
+                            }
+                        )]
+                    })
+                }
+            }
+        }
+
+        let response = await message.reply({ embeds: [process_embed] })
+
+        const session = await createSession(script)
+        if (!session.sessionId) return sendErrorMessage([session.error || "Unable to create session", "Error", session.error_name || "Unknown Error"], message)
+
+        console.log(`New session created by ${message.author.tag}: ${session.sessionId}`)
+        process_embed.data.fields[0].value = `${getEmoji("check")} Session created! ${hyperlink("[open]", config.SESSION_URL + session.sessionId)}\n${getEmoji("loading")} Obfuscating script...`
+        await response.edit({
+            embeds: [process_embed]
+        })
+
+        const obfuscate_script = await obfuscateScript(script)
+        if (obfuscate_script.message && !obfuscate_script.code) return sendErrorMessage([obfuscate_script.message, "Error", "obfuscation"], message)
+        process_embed.data.fields[0].value = `${getEmoji("check")} Session created! ${hyperlink("[open]", config.SESSION_URL + session.sessionId)}\n${getEmoji("check")} Script obfuscated! ${hyperlink("[open]", config.SESSION_URL + obfuscate_script.sessionId)}\n${getEmoji("loading")} Creating attachment file...`
+        await response.edit({
+            embeds: [process_embed]
+        })
+        console.log(`Script by ${message.author.tag} successfully obfuscated: ${obfuscate_script.sessionId}`)
+
+        const file_attachment = createFileAttachment(Buffer.from(obfuscate_script.code))
+        if (typeof file_attachment != "object") return sendErrorMessage([file_attachment.error || "Unable to create file attachment.", "Error", file_attachment.error_name], message)
+
+        process_embed.data.fields[0].value = `${getEmoji("check")} Session created! ${hyperlink("[open]", config.SESSION_URL + session.sessionId)}\n${getEmoji("check")} Script obfuscated! ${hyperlink("[open]", config.SESSION_URL + obfuscate_script.sessionId)}\n${getEmoji("check")} Attachment file created!`
+        await response.edit({
+            embeds: [process_embed]
+        })
+        await message.reply({
+            files: [file_attachment],
+        })
     }
 }
--- utils/obfuscate-util.js
+++ utils/obfuscate-util.js
@@ -1,3 +1,9 @@
+const fetch = require("node-fetch")
+const { API_URL } = require("../.config")
+const { sendErrorMessage } = require("../utils/command")
+const { createEmbed } = require("../utils/embed")
+const { AttachmentBuilder } = require("discord.js")
+
 module.exports = {
     /**
      * @description Checks if a string contains a discord webhook
@@ -10,8 +16,50 @@ module.exports = {
      */
     hasCodeblock: function (string) { return /^([`])[`]*\1$|^[`]/mg.test(string) },
     /**
+     * Parses all discord webhooks in a string
+     * @param { String } string 
+     */
+    parseWebhooks: function (string) { return string.match(/.*https:\/\/.+\/api\/webhooks\/[0-9]+\/.*/gm) },
+    /**
      * @description Parses a discord codeblock. (Removes all codeblock related characters)
      * @param { String } string 
      */
     parseCodeblock: function (string) { return string.replace(/(^`\S*)|`+.*/mg, "").trim() },
+    /**
+     * @description Creates a luaobfuscator.com session with the given script
+     * @param { String } script 
+     */
+    createSession: async function (script) {
+        if (!script || typeof script != "string") return { error: "Unable to create session. (invalid script)", error_name: "createSession" }
+        const response = await fetch(`${API_URL}newscript`, { method: "POST", body: script })
+        return response.json()
+    },
+    /**
+     * Obfuscates the given session and config
+     * @param { String } session 
+     * @param { { String: Object | boolean | String } } config 
+     */
+    manualObfuscateScript: async function (session, config = {}) {
+        if (!session || typeof session != "string") return { error: "Unable to obfuscatescript. (invalid session)", error_name: "manualObfuscateScript" }
+        const response = await fetch(`${API_URL}obfuscate`, { method: "POST", body: JSON.stringify(config), headers: { sessionId: session, apiKey: process.env.API_KEY } })
+        return response.json()
+    },
+    /**
+     * Obfuscates a script
+     * @param { String } script 
+     */
+    obfuscateScript: async function (script) {
+        if (!script || typeof script != "string") return { error: "Unable to obfuscatescript. (invalid script)", error_name: "obfuscateScript" }
+        const response = await fetch(`${API_URL}one-click/hard`, { method: "POST", body: script, headers: { apiKey: process.env.API_KEY } })
+        return response.json()
+    },
+    /**
+     * Creates a discord file attachment with the given buffer content
+     * @param { Buffer } content 
+     */
+    createFileAttachment: function (content) {
+        if (!content || !Buffer.isBuffer(content)) return { error: "Unable to create file attachment. (invalid buffer)", error_name: "obfuscateScript" }
+        const attachment = new AttachmentBuilder(content, { name: "obfuscated.lua" })
+        return attachment
+    }
 }
