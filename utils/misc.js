0494f7b6c53bcff20e7799ea3b8afa1507884915
Flo <floda854@gmail.com>
2023-05-06 14:33:16 +0200

    added formatUptime function

--- utils/misc.js
+++ utils/misc.js
@@ -1,12 +1,21 @@
+const moment = require("moment")
+
+require("moment-duration-format")
+
 module.exports = {
     /**
      * @description Gets a emoji with the given name
      * @param {String} name
      */
-    getEmoji: function(name) {
+    getEmoji: function (name) {
         if (!name || !global.client) return
         let emoji = global.client.emojis.cache.find(emoji => emoji.name === name)
 
         return emoji
-    }
+    },
+    /**
+     * Formats milliseconds to readable time
+     * @param { Number } ms Milliseconds
+     */
+    formatUptime: function (ms = 0) { return moment.duration(ms).format(" D [days], H [hrs], m [mins], s [secs]") }
 }
