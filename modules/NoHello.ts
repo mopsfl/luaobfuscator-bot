cf0e679d88d8d8984859ba9701604b339eb50bd5
mopsfl <floda854@gmail.com>
2023-08-23 17:03:43 +0200

    changed operator to >= for hohello thing

--- modules/NoHello.ts
+++ modules/NoHello.ts
@@ -12,7 +12,7 @@ const nohello_words = [
 export default function (message: Message) {
     if (message.author.bot || message.channel.isDMBased()) return
     nohello_words.forEach(word => message.content = message.content.replace(new RegExp(`${word}|\\s+`, "gm"), ""))
-    const nohello = message.mentions.users.size == 1 && message.content.replace(/\<@\d+\>/gm, "").length <= 0
+    const nohello = message.mentions.users.size >= 1 && message.content.replace(/\<@\d+\>/gm, "").length <= 0
     if (nohello) message.reply(`https://nohello.net`)
     return nohello
 }
