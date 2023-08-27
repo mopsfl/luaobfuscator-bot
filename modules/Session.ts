31979eb515bbdce4b1c0e4a178af74f29c74d897
mopsfl <floda854@gmail.com>
2023-08-27 13:48:41 +0200

    fixed sessions

--- modules/Session.ts
+++ modules/Session.ts
@@ -5,10 +5,11 @@ export default class Session {
     constructor() { }
 
     async Create(tll_seconds: number = null) {
-        const sid = randomUUID()
-        const session_ids: Array<any> = await self.cache.get("stats_session_ids")
+        let sid = randomUUID(),
+            session_ids: Array<any> = await self.cache.get("stats_session_ids")
+        if (typeof (session_ids) != "object") session_ids = []
         session_ids.push(sid)
-        await self.cache.set("stats_session_ids", session_ids, tll_seconds * 1000)
+        await self.cache.set("stats_session_ids", session_ids, tll_seconds != null ? tll_seconds * 1000 : undefined)
         return sid
     }
 }
