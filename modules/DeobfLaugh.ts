b0655fff77fdce9a104db8bba9d7f6f1e77c9b37
mopsfl <floda854@gmail.com>
2024-03-05 18:40:11 +0100

    updated deobf luagh detection

--- modules/DeobfLaugh.ts
+++ modules/DeobfLaugh.ts
@@ -1,26 +1,44 @@
+// todo: make it more cleaner xd
+
 import { Message } from "discord.js";
 import * as self from "../index";
 
-const deobf_words = [
-    "need", "must", "have", "script", "please", "how", "pleaes", "pls", "?", "can"
+const reference = [
+    ["I", "me", "my", "own"],
+    ["need", "must", "have", "should", "will", "want"],
+    ["deobfuscator", "deobf", "de-obf", "de-obfuscator", "deobfuscating", "deobfsc", "deobfuscat", "unobfuscate", "un obfuscate"]
 ]
 
 const LaughGifUrl = "https://tenor.com/view/haha-gif-6815166390687140758"
 
 export default function (message: Message) {
     if (message.author.bot || message.channel.isDMBased()) return
-    let msg = message.content.replace(/\<@\d+\>/gm, "").replace(/^\s+/gm, ""),
-        hasDeobfWord = false
+    const msg = message.content.toLowerCase().replace(/\<@\d+\>/gm, "").trim(); // Trim whitespace
+    let referenced = false,
+        hasDeobfWord = false,
+        hasDeobfTerm = false
 
-    for (let i = 0; i < deobf_words.length; i++) {
-        if (msg.includes(deobf_words[i])) {
+    for (const keyword of reference[0]) {
+        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
+            referenced = true;
+            break;
+        }
+    }
+    if (!referenced) return;
+    for (const keyword of reference[1]) {
+        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
             hasDeobfWord = true;
             break;
         }
     }
-
-    let deobfLaugh = msg.includes("deobf") && hasDeobfWord;
-    deobfLaugh && message.reply(LaughGifUrl);
-    return deobfLaugh;
+    if (!hasDeobfWord) return;
+    for (const keyword of reference[2]) {
+        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
+            hasDeobfTerm = true;
+            break;
+        }
+    }
+    hasDeobfTerm && message.reply(LaughGifUrl);
+    return hasDeobfTerm
 }
 
