6b21adcd652d920522c149d6bddce7779c5eda5a
mopsfl <floda854@gmail.com>
2024-05-19 20:10:34 +0200

    fixed false detections for "DeobfLaugh" and added ignored roles

--- modules/DeobfLaugh.ts
+++ modules/DeobfLaugh.ts
@@ -1,44 +1,32 @@
 // todo: make it more cleaner xd
 
-import { Message } from "discord.js";
+import { Message, PermissionFlagsBits } from "discord.js";
 import * as self from "../index";
 
 const reference = [
     ["i", "me", "my", "own", "can"],
-    ["need", "must", "have", "should", "will", "want", "someone", "u", "you"],
+    ["need", "must", "have", "should", "will", "want", "someone", "you"],
     ["deobfuscator", "deobf", "de-obf", "de-obfuscator", "deobfuscating", "deobfsc", "deobfuscat", "unobfuscate", "decompile", "decrypt", "unencrypt"]
+], ignored_roles = [
+    "1112411674826248274",
+    "1127902057257500785"
 ]
 
 const LaughGifUrl = "https://tenor.com/view/haha-gif-6815166390687140758"
 
 export default function (message: Message) {
-    if (message.author.bot || message.channel.isDMBased()) return
-    const msg = message.content.toLowerCase().replace(/\<@\d+\>/gm, "").trim(); // Trim whitespace
-    let referenced = false,
-        hasDeobfWord = false,
-        hasDeobfTerm = false
+    if (message.author.bot || message.channel.isDMBased()) return;
+    if (message.member.roles.cache.find(role => ignored_roles.includes(role.id)) || message.member.permissions.has(PermissionFlagsBits.Administrator)) return;
 
-    for (const keyword of reference[0]) {
-        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
-            referenced = true;
-            break;
-        }
+    const msg = message.content.toLowerCase().replace(/\<@\d+\>/gm, "").trim();
+
+    const ref1 = reference[0].some(keyword => msg.includes(keyword));
+    const ref2 = reference[1].some(keyword => msg.includes(keyword));
+    const ref3 = reference[2].some(keyword => msg.includes(keyword));
+
+    if (ref1 && ref2 && ref3) {
+        message.reply(LaughGifUrl);
     }
-    if (!referenced) return;
-    for (const keyword of reference[1]) {
-        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
-            hasDeobfWord = true;
-            break;
-        }
-    }
-    if (!hasDeobfWord) return;
-    for (const keyword of reference[2]) {
-        if ((msg.toLocaleLowerCase()).includes(keyword.toLocaleLowerCase())) {
-            hasDeobfTerm = true;
-            break;
-        }
-    }
-    hasDeobfTerm && message.reply(LaughGifUrl);
-    return hasDeobfTerm
+
+    return true
 }
-
