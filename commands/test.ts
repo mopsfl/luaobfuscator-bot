459077417e73430142806d808d8f660e241ff8c3
mopsfl <floda854@gmail.com>
2025-10-14 18:38:06 +0200

    test thing

--- commands/test.ts
+++ commands/test.ts
@@ -1,38 +1,25 @@
-import { codeBlock, Colors, inlineCode, PermissionFlagsBits } from "discord.js";
+import { PermissionFlagsBits } from "discord.js";
 import { Command, CommandNode } from "../modules/CommandHandler"
+import { commandHandler } from "../index";
 import Embed from "../modules/Misc/Embed";
-import { client, commandHandler } from "../index";
-import config from "../config";
-import Utils from "../modules/Utils";
+import Chart from "../modules/StatusDisplay/Chart";
+import ObfuscatorStats from "../modules/ObfuscatorStats";
 
 class CommandConstructor implements CommandNode {
     name = ["test"]
     category = commandHandler.CommandCategories.Misc
     description = "Test Command"
     permissions = [PermissionFlagsBits.Administrator]
-    public_command = false
 
     callback = async (command: Command) => {
+        const embed = Embed({
+            title: "chart test",
+            image: Chart.Create(await ObfuscatorStats.Parse(50), 50).toString(),
+            timestamp: true
+        })
+
         command.message.reply({
-            embeds: [Embed({
-                color: Colors.Yellow,
-                title: "Obfuscation Process",
-                timestamp: true,
-                footer: {
-                    text: `Lua Obfuscator Bot - made by mopsfl`,
-                    iconURL: config.icon_url,
-                },
-                fields: [
-                    { name: "Script:", value: `-# ${Utils.FormatBytes(new TextEncoder().encode("a".repeat(Math.random() * 10000)).length)}`, inline: true },
-                    { name: "Obfuscation Type:", value: `-# Default`, inline: true },
-                    { name: "Process ID:", value: `-# 5dc76dfc`, inline: true },
-                    { name: "Process State:", value: `-# FINISHED`, inline: true },
-                    { name: "Process Time:", value: `-# 4714ms`, inline: true },
-                    { name: "\u200B", value: "\u200B", inline: true },
-                    { name: "Session:", value: `-# ${inlineCode("1R8cV6zW462h613D72NdDE5y9e1sDQ3kEd8vn9rrK5H3xrA008CT0iWWatOyD1k1")}`, inline: false },
-                    { name: `Process:`, value: codeBlock("-"), inline: false },
-                ]
-            })]
+            embeds: [embed]
         })
     }
 }
