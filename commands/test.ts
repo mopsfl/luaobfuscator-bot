9fb4ade101258e2a9cf07af64f68ae6baf04c141
mopsfl <floda854@gmail.com>
2025-11-02 22:53:25 +0100

    added some more test command features

--- commands/test.ts
+++ commands/test.ts
@@ -1,6 +1,6 @@
-import { PermissionFlagsBits } from "discord.js";
+import { codeBlock, inlineCode, PermissionFlagsBits } from "discord.js";
 import { Command, CommandNode } from "../modules/CommandHandler"
-import { commandHandler } from "../index";
+import { commandHandler, statusDisplayController } from "../index";
 import Embed from "../modules/Misc/Embed";
 import Chart from "../modules/StatusDisplay/Chart";
 import ObfuscatorStats from "../modules/ObfuscatorStats";
@@ -12,15 +12,39 @@ class CommandConstructor implements CommandNode {
     permissions = [PermissionFlagsBits.Administrator]
 
     callback = async (command: Command) => {
-        const embed = Embed({
-            title: "chart test",
-            image: Chart.Create(await ObfuscatorStats.Parse(50), 50).toString(),
-            timestamp: true
-        })
+        if (!command.arguments[0]) return await command.message.reply("please provide a test command name as an argument!")
+        switch (command.arguments[0]) {
+            case "chart": {
+                const embed = Embed({
+                    title: "chart test",
+                    image: Chart.Create(await ObfuscatorStats.Parse(50), 50).toString(),
+                    timestamp: true
+                })
 
-        command.message.reply({
-            embeds: [embed]
-        })
+                await command.message.reply({ embeds: [embed] })
+                break;
+            }
+            case "cbembed": {
+                const embed = Embed({
+                    fields: [
+                        { name: "val1", value: codeBlock("value1"), inline: true },
+                        { name: "val2", value: codeBlock("value2"), inline: true },
+                        { name: "val3", value: codeBlock("value3"), inline: true },
+                    ]
+                })
+                await command.message.reply({ embeds: [embed] })
+                break;
+            }
+            case "oalert": case "ol": case "outagealert": {
+                if (command.arguments[1]) {
+                    statusDisplayController.SendAlertMessage(await statusDisplayController.GetOutage(command.arguments[1]))
+                } else statusDisplayController.SendAlertMessage(statusDisplayController.lastOutage)
+                break;
+            }
+            default:
+                await command.message.reply(`unknown test command name ${inlineCode(command.arguments[0])}`)
+                break;
+        }
     }
 }
 
